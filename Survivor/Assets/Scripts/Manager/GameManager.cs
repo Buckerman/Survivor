faf189e159669fee01a8119b86aa736ec9e9a1ad
DucAnh8530 <s25395@pjwstk.edu.pl>
2024-08-06 17:27:05 +0700

    GameManager
    + Game Manager collects all building to bake walls

--- Survivor/Assets/Scripts/Manager/GameManager.cs
+++ Survivor/Assets/Scripts/Manager/GameManager.cs
@@ -2,6 +2,7 @@ using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,7 +11,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private CountdownTimer countdownTimer;
     [SerializeField] private Text defeatGameText;
     [SerializeField] private Text winGameText;
-    [SerializeField] private NavMeshSurface navMeshSurface;
+    [SerializeField] private NavMeshSurface groundSurface;
+    [SerializeField] private string wallTag = "Wall";
+
+    private List<NavMeshSurface> wallSurfaces = new List<NavMeshSurface>();
 
     private void Awake()
     {
@@ -35,13 +39,35 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         defeatGameText.gameObject.SetActive(false);
         winGameText.gameObject.SetActive(false);
-        navMeshSurface.GetComponent<EnemySpawner>().enabled = true;
+        groundSurface.GetComponent<EnemySpawner>().enabled = true;
         countdownTimer.StartTimer();
     }
 
     private void BakeNavMesh()
     {
-        navMeshSurface.BuildNavMesh();
+        groundSurface.BuildNavMesh();
+
+        //Find all buildings
+        GameObject[] buildings = GameObject.FindGameObjectsWithTag("Building");
+        foreach (GameObject building in buildings)
+        {
+            // Find wall objects that are children of the building
+            foreach (Transform child in building.transform)
+            {
+                if (child.CompareTag(wallTag))
+                {
+                    // Add NavMeshSurface to the wall if it doesn't have one already
+                    NavMeshSurface surface = child.gameObject.GetComponent<NavMeshSurface>();
+                    if (surface == null)
+                    {
+                        surface = child.gameObject.AddComponent<NavMeshSurface>();
+                    }
+
+                    wallSurfaces.Add(surface);
+                    surface.BuildNavMesh();
+                }
+            }
+        }
     }
 
     public void EndWave()
