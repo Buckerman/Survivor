d24885ddcbad15f59cf8f0256ac1a9e17fabeb75
DucAnh8530 <s25395@pjwstk.edu.pl>
2024-08-20 16:54:55 +0700

    DamageText  text -> TMP

--- Survivor/Assets/Scripts/DamageText.cs
+++ Survivor/Assets/Scripts/DamageText.cs
@@ -3,33 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+
 public class DamageText : MonoBehaviour
 {
-    private TextMesh _textMesh;
+    private TextMeshPro _textMeshPro;
     private Transform _transform;
     private DamageTextPool _damageTextPool;
 
     private void Awake()
     {
-        _textMesh = GetComponent<TextMesh>();
+        _textMeshPro = GetComponentInChildren<TextMeshPro>();
     }
+
     private void Start()
     {
         Observer.Instance.AddObserver("DisableAllDamageText", DisableAllText);
     }
+
     public void Initialize(DamageTextPool pool)
     {
         _damageTextPool = pool;
     }
+
     public void Setup(int damageAmount, Color color)
     {
-        _textMesh.text = damageAmount.ToString();
-        _textMesh.color = color;
+        _textMeshPro.text = damageAmount.ToString();
+        _textMeshPro.color = color;
     }
+
     private void DisableAllText(object data)
     {
         OnDisable();
     }
+
     private void OnDisable()
     {
         if (_damageTextPool != null)
